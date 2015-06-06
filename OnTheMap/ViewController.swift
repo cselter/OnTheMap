69b6bbc93b112140aaf5269cafffbbef129183c3
Christopher Burgess <cdotburgess@gmail.com>
2015-06-06 10:43:19 -0700

    Empty Project

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Christopher Burgess on 6/6/15.
+//  Copyright (c) 2015 Christopher Burgess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view, typically from a nib.
+     }
+
+     override func didReceiveMemoryWarning() {
+          super.didReceiveMemoryWarning()
+          // Dispose of any resources that can be recreated.
+     }
+
+
+}
+
